bd3a018d7549f434ee4ace4f19602f3a8d016522
TheThreshCarry <mehdichioukh@gmail.com>
2020-11-27 14:21:59 +0100

    First Commit
    First Commit of the Rewind Project

---
+++ Assets/Explosion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+public class Explosion : MonoBehaviour
+{
+    public float radius = 5.0F;
+    public float power = 10.0F;
+    public float lift = 30;
+    public float speed = 10;
+    public bool explode = false;
+    void FixedUpdate()
+    {
+        if (explode)
+        {
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                if (hit.GetComponent<Rigidbody>())
+                {
+                    hit.GetComponent<Rigidbody>().AddExplosionForce(power, explosionPos, radius, lift);
+                }
+            }
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            explode = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            explode = false;
+        }
+    }
+}
---
+++ Assets/RewindControll3D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindControll3D : MonoBehaviour
+{
+    List<Vector3> positions;
+    List<Quaternion> rotations;
+    Rigidbody rb;
+    public bool isRewinding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new List<Vector3>();
+        rotations = new List<Quaternion>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            isRewinding = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            isRewinding = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isRewinding && positions.Count != 2 && rotations.Count != 1)
+        {
+            rb.isKinematic = true;
+            transform.position = positions[0];
+            transform.rotation = rotations[0];
+            positions.RemoveAt(0);
+            rotations.RemoveAt(0);
+            Debug.Log("Working");
+        }
+        else if (positions.Count < Mathf.RoundToInt(5f * 1f / Time.fixedDeltaTime))
+        {
+            rb.isKinematic = false;
+            positions.Insert(0, transform.position);
+            rotations.Insert(0, transform.rotation);
+
+        }
+    }
+}
